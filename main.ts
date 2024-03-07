6ed20cd2a6accc76de1684c4939353481a22a556
mahailahmed <160250995+mahailahmed@users.noreply.github.com>
2024-03-07 11:05:39 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+//--decrement               
+
+                  // first method -1 huga x ki valu ma say
+
+                  let x = 10 
+                  x = x-1 
+                  console.log(x)
+                  
+                                   //second method    ( pre decrement)
+                  
+                    let y = 10 
+                      y = --y 
+                    console.log(y)
+                                   
+                  let z = 10 
+                       z = z-- 
+                    console.log(z--)             //phaly 10 he ayega jb dubara post kren gain value main say -1 huga
+                    console.log(z--)      
+                    console.log(z--)      
+                    console.log(z--)      
+                    console.log(z)      
+                  
